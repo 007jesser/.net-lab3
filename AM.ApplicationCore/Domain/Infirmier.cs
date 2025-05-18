185e5e6e30f822b776c46e4481b9ba8c3fccc31c
jesser-007 <jesser.sallemi@esprit.tn>
2025-05-19 00:10:04 +0100

    Initial commit for .NET Lab 3

---
+++ AM.ApplicationCore/Domain/Infirmier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public enum Specialite
+    {
+        Hematologie,
+        Biochimie,
+        Autre
+    }
+
+    public class Infirmier
+    {
+        public int InfirmierId { get; set; }
+        public string NomComplet { get; set; }
+        public Specialite specialite { get; set; }
+        public ICollection<Patient> Patients { get; set; }
+        public Laboratoire Laboratoire { get; set; }
+
+        [ForeignKey("Laboratoire")]
+        public int LaboratoireFK { get; set; }
+
+        public ICollection<Bilan> Bilans { get; set; }
+    }
+}
